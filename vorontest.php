a09fe9e0500e8544080fe0df67e58bde2d0dff41
Dich <antonlykinov96@gmail.com>
2024-05-31 12:38:30 +0300

    v2

--- vorontest.php
+++ vorontest.php
@@ -1,20 +1,22 @@
 <?php
-  for($i = 1; $i<=10; $i++){
-    echo $i . ' ' .sklonvoron($i) . "\n"; 
-  }
-  function sklonvoron($number, $options = ['Ворона', 'Вороны', 'Ворон']){
-    $index = 1;
-    $varone = $number % 100;
-      if($varone <2){
+    for($i = 0; $i<=100; $i++){
+        echo $i . ' ' .sklonvoron($i) . "\n"; 
+    }
+    function sklonvoron($number, $options = ['Ворона', 'Вороны', 'Ворон']){
         $index = 0;
-      }
-      else{
         $varone = $number % 100;
-        if($varone >=5){
-          $index = 2;
+    if($varone >= 11 && $varone <= 14){
+        $index = 2;
+    }
+    else{
+        $varone = $number % 10;
+        if($varone == 0 || $varone >=5){
+            $index = 2;
         }
-      }
+    elseif($varone !=1){
+        $index = 1;
+        }
+    }
     return $options[$index];
-  }
-
+    }
  ?>
